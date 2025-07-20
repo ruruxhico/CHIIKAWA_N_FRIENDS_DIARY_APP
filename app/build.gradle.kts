4bfb68a203c07fdce2220f15070936af3704e558
ruruxhico <daniellejanm.15@gmail.com>
2025-07-20 16:05:14 +0800

    new sdk in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.chiikawa_n_friends_diary_app"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
