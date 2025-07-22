3b71f36e70539ff0fe105b058e7ebfa92eefa623
reddddd10 <ibarrientos1010@gmail.com>
2025-07-22 15:04:44 +0800

    Made changes to MainMenu xml. Unsure if Notes and Profile need to be changed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.chiikawa_n_friends_diary_app"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
